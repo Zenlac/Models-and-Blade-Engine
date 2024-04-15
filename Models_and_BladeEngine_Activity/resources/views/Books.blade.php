50edd5fe100f2cae7b1608412b0865022fc73784
Zenlac <caleleizengrillera@gmail.com>
2024-04-16 01:10:42 +0800

    Created Views & Partials with the installation of tailwindcss

---
+++ Models_and_BladeEngine_Activity/resources/views/Books.blade.php
@@ -0,0 +1,5 @@
+@include('partials.header');
+
+<h1>Books</h1>
+
+@include('partials.footer');
---
+++ Models_and_BladeEngine_Activity/resources/views/partials/header.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+    <title>Models and Blade Engine</title>
+</head>
+<body>
+    <p class="text-3xl font-bold underline">
+        this is the header
+    </p>
